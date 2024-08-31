9a11017339ec7cca48a7659595bb86b3b93e6316
Pringled <thomas123@live.nl>
2024-08-31 08:35:48 +0200

    Moved fixture to conftest

--- tests/test_evaluation.py
+++ tests/test_evaluation.py
@@ -1,34 +1,9 @@
 from pathlib import Path
-from typing import Any, Sequence
-from unittest.mock import create_autospec
 
 import pytest
-import torch
 from mteb.encoder_interface import Encoder
-from mteb.model_meta import ModelMeta
 
 from evaluation import CustomMTEB, TaskType, get_tasks
-
-
-@pytest.fixture
-def mock_encoder() -> Encoder:
-    """Return a mock encoder that follows the Encoder protocol."""
-    mock_encoder = create_autospec(Encoder, instance=True)
-
-    # Mock the encode method
-    def mock_encode(sentences: Sequence[str], prompt_name: str | None = None, **kwargs: Any) -> torch.Tensor:
-        """Return random embeddings for the sentence."""
-        return torch.rand(len(sentences), 300)
-
-    # Set the side effect of the mock
-    mock_encoder.encode.side_effect = mock_encode
-
-    # Set the model meta
-    mock_encoder.mteb_model_meta = ModelMeta(
-        name="mock_model_name", revision="mock_revision", release_date=None, languages=None
-    )
-
-    return mock_encoder
 
 
 def test_evaluation(mock_encoder: Encoder, tmp_path: Path) -> None:
